30c125096a7694375f828dd1ad28feb6a19dc6f5
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 12:29:25 +0000

    add class constructor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./styles/App.css";
 import React, { Component } from "react";
+import {
+  DefaultPersonal,
+  DefaultExperience,
+  DefaultEducation,
+} from "./components/data/Default";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      personal: [DefaultPersonal],
+      experience: DefaultExperience.map((obj) => ({ ...obj })),
+      education: DefaultEducation.map((obj) => ({ ...obj })),
+    };
+  }
+  
   render() {
     return <div className="App"></div>;
   }
