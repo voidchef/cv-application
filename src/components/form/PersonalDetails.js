6df1ea5b1e09b2b311e2ab358a63c0bbd0eba3f6
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 13:05:44 +0000

    add personal details section (form) component

---
+++ src/components/form/PersonalDetails.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import InputCreator from "../utils/InputCreator";
+
+class PersonalDetails extends Component {
+  render() {
+    const { data, Change } = this.props;
+    return (
+      <div className="PersonalDetails">
+        <h2>Personal Details</h2>
+        {Object.entries(data[0]).map(([key, value]) => {
+          if (key !== "id") {
+            if (key === "Description") {
+              return (
+                <InputCreator
+                  type="textarea"
+                  group="personal"
+                  name={key}
+                  value={value}
+                  Change={Change}
+                  id={data[0].id}
+                />
+              );
+            } else {
+              return (
+                <InputCreator
+                  type="text"
+                  group="personal"
+                  name={key}
+                  value={value}
+                  Change={Change}
+                  id={data[0].id}
+                />
+              );
+            }
+          }
+        })}
+      </div>
+    );
+  }
+}
+
+export default PersonalDetails;
