b9970f24ce3c16dd0cfef7009b6f881728edb598
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 14:07:42 +0000

    add alt property to images

--- src/components/view/PersonalDetailsView.js
+++ src/components/view/PersonalDetailsView.js
@@ -15,18 +15,21 @@ class PersonalDetailsView extends Component {
             <div className="phone">
               <img
                 src={process.env.PUBLIC_URL + "/img/phone-square-solid.svg"}
+                alt="phone"
               ></img>
               {data[0]["Phone"]}
             </div>
             <div className="email">
               <img
                 src={process.env.PUBLIC_URL + "/img/envelope-square-solid.svg"}
+                alt="envelope"
               ></img>
               {data[0]["Email"]}
             </div>
             <div className="location">
               <img
                 src={process.env.PUBLIC_URL + "/img/map-marker-alt-solid.svg"}
+                alt="map"
               ></img>
               {data[0]["Location"]}
             </div>
