a318eac080e88cc12ef5c0fe6aaccf73fbec9f53
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 12:23:05 +0000

    add cv data template

---
+++ src/components/data/Data.js
@@ -0,0 +1,31 @@
+import uniquid from "uniquid";
+
+const Data = [
+  {
+    Name: "",
+    Title: "",
+    Phone: "",
+    Email: "",
+    Location: "",
+    Description: "",
+    id: uniquid(),
+  },
+  {
+    Company: "",
+    Position: "",
+    "Start Date": "",
+    "End Date": "",
+    Description: "",
+    id: uniquid(),
+  },
+  {
+    Course: "",
+    University: "",
+    "Start Date": "",
+    "End Date": "",
+    Description: "",
+    id: uniquid(),
+  },
+];
+
+export { Data };
