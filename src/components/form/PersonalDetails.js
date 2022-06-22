e4b31799c617ca471bbaf9b5cf5294b35ebc2574
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-22 12:53:08 +0000

    use mui box and typography in personal details (form)

--- src/components/form/PersonalDetails.js
+++ src/components/form/PersonalDetails.js
@@ -1,42 +1,34 @@
 import React, { Component } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import InputCreator from "../utils/InputCreator";
+import uniqid from "uniqid";
 
 class PersonalDetails extends Component {
   render() {
     const { data, Change } = this.props;
     return (
-      <div className="PersonalDetails">
-        <h2>Personal Details</h2>
+      <Box>
+        <Typography variant="h2" component="div">
+          Personal Details
+        </Typography>
         {Object.entries(data[0]).map(([key, value]) => {
           if (key !== "id") {
-            if (key === "Description") {
-              return (
-                <InputCreator
-                  type="textarea"
-                  group="personal"
-                  name={key}
-                  value={value}
-                  Change={Change}
-                  id={data[0].id}
-                />
-              );
-            } else {
-              return (
-                <InputCreator
-                  type="text"
-                  group="personal"
-                  name={key}
-                  value={value}
-                  Change={Change}
-                  id={data[0].id}
-                />
-              );
-            }
+            return (
+              <InputCreator
+                key={uniqid()}
+                group="personal"
+                name={key}
+                value={value}
+                Change={Change}
+                id={data[0].id}
+              />
+            );
           } else {
-            return null
+            return null;
           }
         })}
-      </div>
+      </Box>
     );
   }
 }
