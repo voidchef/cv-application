702c274fa2ec56ad30039ce4746fb4e9d7d577d2
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-22 12:20:51 +0000

    use CssBaseline from mui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./styles/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+  <CssBaseline />
     <App />
   </React.StrictMode>
 );
