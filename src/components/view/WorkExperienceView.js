21608a825b670c97ff93cbd5f9a38c40b559702c
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-22 13:11:04 +0000

    use mui in work experience (view)

--- src/components/view/WorkExperienceView.js
+++ src/components/view/WorkExperienceView.js
@@ -1,31 +1,49 @@
-import "../../styles/WorkExperienceView.css";
 import React, { Component } from "react";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import uniqid from "uniqid";
 
 class WorkExperienceView extends Component {
   render() {
     const { data } = this.props;
     return (
-      <div className="WorkExperienceView">
-        <h3>WORK EXPERIENCE</h3>
+      <Stack direction="column" spacing={1}>
+        <Typography variant="h3" component="div">
+          WORK EXPERIENCE
+        </Typography>
         {data.map((details) => {
           return (
-            <div className="experience">
-              <div className="experienceHeading">
-                <h4>{details["Company"]}</h4>
-                <div className="experienceDetails">
-                  {details["Position"]} |{" "}
-                  <span className="duration">
-                    {details["Start Date"]} – {details["End Date"]}
-                  </span>
-                </div>
-              </div>
-              <div className="experienceDescription">
-                {details["Description"]}
-              </div>
-            </div>
+            <Grid container direction="column" key={uniqid()}>
+              <Grid
+                container
+                item
+                direction="row"
+                justifyContent="space-between"
+              >
+                <Grid item>
+                  <Typography variant="h4" component="div" fontWeight={600}>
+                    {details.Company}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h4" component="div">
+                    {details.Position} |{" "}
+                    <strong>
+                      {details["Start Date"]} – {details["End Date"]}
+                    </strong>
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Typography variant="h4" component="div">
+                  {details.Description}
+                </Typography>
+              </Grid>
+            </Grid>
           );
         })}
-      </div>
+      </Stack>
     );
   }
 }
