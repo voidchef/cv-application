9c95735c78ead2202f54a669dcf2255a8bcf4187
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-25 10:46:46 +0000

    convert index.js to ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./styles/index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
   <CssBaseline />
